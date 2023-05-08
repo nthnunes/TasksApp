46529fcf0d6b92ac1682dc077ac5008036fda630
Nathan Nunes <nathanpedronunes@hotmail.com>
2023-05-08 20:30:51 -0300

    Fix: description input & added labels

--- tasks/src/pages/NewTask/index.js
+++ tasks/src/pages/NewTask/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import firebase from "../../config/firebaseconfig";
 import styles from "./style";
@@ -23,34 +23,40 @@ export default function NewTask({ navigation, route }) {
 
   function handleContentSizeChange(event) {
     // atualiza a altura da entrada com base no tamanho do conteúdo
-    setHeight(event.nativeEvent.contentSize.height);
+    setHeight(event.nativeEvent.contentSize.height + 15);
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.label}>Say something about it</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Title Here"
-        onChangeText={setTitle}
-      />
-      <TextInput
-        style={[styles.input, { height }]} // aplica a altura dinamicamente
-        placeholder="Description..."
-        onChangeText={setDescription}
-        value={description}
-        multiline={true} // permite várias linhas de texto
-        onContentSizeChange={handleContentSizeChange} // atualiza a altura
-        scrollEnabled={false}
-      />
-      <TouchableOpacity
-        style={styles.buttonNewTask}
-        onPress={() => {
-          addTask();
-        }}
-      >
-        <FontAwesome name="check" size={26} color="#fff" />
-      </TouchableOpacity>
-    </View>
+    <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.container}
+    >
+      <View style={styles.container}>
+        <Text style={styles.label}>Task Title</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Type a title..."
+          onChangeText={setTitle}
+        />
+        <Text style={styles.label}>Description</Text>
+        <TextInput
+          style={[styles.input, { height }]} // aplica a altura dinamicamente
+          placeholder="Type a description..."
+          onChangeText={setDescription}
+          value={description}
+          multiline={true} // permite várias linhas de texto
+          onContentSizeChange={handleContentSizeChange} // atualiza a altura
+          scrollEnabled={false}
+        />
+        <TouchableOpacity
+          style={styles.buttonNewTask}
+          onPress={() => {
+            addTask();
+          }}
+        >
+          <FontAwesome name="check" size={26} color="#fff" />
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
