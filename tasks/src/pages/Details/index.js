6abc751e689295e3944f4d62116e081ba1d56bda
Nathan Nunes <nathanpedronunes@hotmail.com>
2023-05-09 13:51:03 -0300

    Update: copy like new task

--- tasks/src/pages/Details/index.js
+++ tasks/src/pages/Details/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { View, Text, TextInput, TouchableOpacity } from "react-native"
+import { View, Text, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView } from "react-native"
 
 import { FontAwesome } from "@expo/vector-icons"
 import firebase from "../../config/firebaseconfig"
@@ -21,41 +21,51 @@ export default function Details({ navigation, route }){
 
     function handleContentSizeChange(event) {
         // atualiza a altura da entrada com base no tamanho do conteúdo
-        setHeight(event.nativeEvent.contentSize.height);
+        if(Platform.OS != "ios"){
+            setHeight(event.nativeEvent.contentSize.height)
+        }else{
+            setHeight(event.nativeEvent.contentSize.height + 15)
+        }
     }
 
     return(
-        <View style={styles.container}>
-            <Text style={styles.label}>Say something about it</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="Title Here"
-                onChangeText={setTitleEdit}
-                value={titleEdit}
-            />
-            <TextInput
-                style={[styles.input, { height }]}
-                placeholder="Description..."
-                onChangeText={setDescriptionEdit}
-                value={descriptionEdit}
-                multiline={true} // permite várias linhas de texto
-                onContentSizeChange={handleContentSizeChange} // atualiza a altura
-                scrollEnabled={false}
-            />
-            <Text style={styles.create}>{route.params.create}</Text>
-            <Text style={styles.concluded}>{route.params.concluded}</Text>
-            <TouchableOpacity
-                style={styles.buttonNewTask}
-                onPress={() => {
-                    editTask(descriptionEdit, idTask)
-                }}
-            >
-                <FontAwesome
-                    name="check"
-                    size={26}
-                    color="#fff"
+        <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.container}
+        >
+            <View style={styles.container}>
+                <Text style={styles.label}>Task Title</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Type a title..."
+                    onChangeText={setTitleEdit}
+                    value={titleEdit}
                 />
-            </TouchableOpacity>
-        </View>
+                <Text style={styles.label}>Description</Text>
+                <TextInput
+                    style={[styles.input, { height }]}
+                    placeholder="Type a description..."
+                    onChangeText={setDescriptionEdit}
+                    value={descriptionEdit}
+                    multiline={true} // permite várias linhas de texto
+                    onContentSizeChange={handleContentSizeChange} // atualiza a altura
+                    scrollEnabled={false}
+                />
+                <Text style={styles.create}>{route.params.create}</Text>
+                <Text style={styles.concluded}>{route.params.concluded}</Text>
+                <TouchableOpacity
+                    style={styles.buttonNewTask}
+                    onPress={() => {
+                        editTask(descriptionEdit, idTask)
+                    }}
+                >
+                    <FontAwesome
+                        name="check"
+                        size={26}
+                        color="#fff"
+                    />
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     )
 }
